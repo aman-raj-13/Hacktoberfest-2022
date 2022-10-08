49cd75d5a30d46cb2b2c6332857fdb83df80e996
rj-33 <73634047+rj-33@users.noreply.github.com>
2022-10-08 23:31:49 +0530

    Update BinarySearch.java

--- Java/BinarySearch.java
+++ Java/BinarySearch.java
@@ -4,17 +4,17 @@ class BinarySearch {
         if (r >= l) {
             int mid = l + (r - l) / 2;
  
-            // If the element is present at the
+            // if the element is present at the
             // middle itself
             if (arr[mid] == x)
                 return mid;
  
-            // If element is smaller than mid, then
+            // if element is smaller than mid, then
             // it can only be present in left subarray
             if (arr[mid] > x)
                 return binarySearch(arr, l, mid - 1, x);
  
-            // Else the element can only be present
+            // else the element can only be present
             // in right subarray
             return binarySearch(arr, mid + 1, r, x);
         }
@@ -23,6 +23,7 @@ class BinarySearch {
  
     public static void main(String args[])
     {
+        
         BinarySearch ob = new BinarySearch();
         int arr[] = { 2, 3, 4, 10, 40 };
         int n = arr.length;
@@ -32,5 +33,6 @@ class BinarySearch {
             System.out.println("Element not present");
         else
             System.out.println("Element found at index " + result);
+
     }
 }
